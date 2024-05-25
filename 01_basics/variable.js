896d01f4374f0daa1950393959144a752cfd78f3
jayesh rewani <rehwanij@gmail.com>
2024-05-25 10:32:50 +0000

    config

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+//investigation
+const accountid = "15520"
+let accountpassword="20@00"
+let accountemail="gc@gamil.com"
+accountcity="ajmer"
+// var name variable bhi hota h but not recommened// const variable mai  value constant hoti h use app update yah change nhi kar sakth
+// accountid="2"// not allowed
+accountpassword="44"
+accountemail="jay@gmail.com"
+accountcity="kisangaer"
+
+console.log(accountid)
+console.table([accountemail,accountid,accountpassword,accountcity])
