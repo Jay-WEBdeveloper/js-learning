246a5a5471e698f69e697bc638f03a86756a3c17
jayesh rewani <rehwanij@gmail.com>
2024-05-25 16:09:15 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+
+//alert(3+3) // we are using node,js not brower
+
+console.log(3+3)
+console.log("jayesh")
+
+let name="jayesh"
+let age=8
+let isloggedin=false
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+// data type 
+console.log(typeof name)// string
+console.log(typeof age);// number
+console.log(typeof isloggedin);//boolean
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
