5b516bf71c9a75017a2ee56f772ee79cb01a38fa
jayesh rewani <rehwanij@gmail.com>
2024-05-27 15:59:02 +0000

    config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,45 @@
+// number to string&boolean conversion
+let source=22;
+
+console.log(typeof source);
+let stringnumber= String(source);
+console.log(typeof stringnumber);
+console.log(stringnumber);
+// conversion on boolean
+let booleanNumber= Boolean(source)
+console.log(typeof booleanNumber);
+console.log(booleanNumber);
+
+// string to number & boolean
+let city="jaipur";
+
+console.log(typeof city);
+
+let numberstring= Number(city);
+console.log(typeof numberstring);
+console.log(numberstring);//Nan(not a number)
+// boolean conversion
+let area="roopangarh";
+
+console.log(typeof area);
+let booleanstring=Boolean(area);
+console.log(typeof booleanstring);
+console.log(booleanstring);
+
+// boolean conversion to number&string
+
+let inloggedIn=true;
+
+console.log(inloggedIn);
+
+let stringboolean=String(inloggedIn);
+console.log(typeof stringboolean);
+console.log(stringboolean);
+
+// conversion in number
+let Inloggedin=false;
+
+console.log(typeof Inloggedin);
+let numberboolean= Number(Inloggedin);
+console.log(typeof numberboolean);
+console.log(numberboolean);
