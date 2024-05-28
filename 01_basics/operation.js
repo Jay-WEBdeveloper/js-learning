322014fe490104c0699667e566404d69765161cd
jayesh rewani <rehwanij@gmail.com>
2024-05-28 08:40:38 +0000

    config done

---
+++ 01_basics/operation.js
@@ -0,0 +1,37 @@
+// prefix increment opreation
+
+let x=5;
+let y=++x;
+
+console.log(x);// x is increment value is 6, then y assiend of (x)
+console.log(y);
+
+// prefix decrement opreations
+
+let a= 10;
+let b= --a;
+
+console.log(a);// x  is decrement value is 9,then y assien of value of (x)
+console.log(b);
+
+// postfix operations increment/decrement
+let z=4;
+let q=z++;
+
+console.log(z);// z is increment value is 5, but q is const of z before value
+console.log(q);
+
+// postfix decrement 
+
+let gamescore=100;
+gamescore--;
+
+console.log(gamescore);
+
+// string operation
+
+let boy1=20;
+let boy2=2;
+let boy3=boy1+boy2;
+
+console.log(boy3);
